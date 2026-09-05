74cc42058cebfd108738282a4c0dc6f9e6ea3643
Abhishek-Sparke <Abhishek-Sparke@users.noreply.github.com>
2026-09-05 23:23:37 +0530

    Add rolling side rails animation to community page

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,11 +1,15 @@
 import CommunityFeed from '../components/community-feed';
 import PublicNavbar from '../components/public-navbar';
+import SideIconRail from '../components/side-icon-rail';
 
 export const dynamic = 'force-dynamic';
 
 export default function CommunityPage() {
-  return <main>
+  return <main style={{ position: 'relative', overflowX: 'clip' }}>
     <PublicNavbar returnTo="/community"/>
+    {/* Infinite Side Visual Animation Rails */}
+    <SideIconRail side="left" />
+    <SideIconRail side="right" />
     <section className="communityHero shell">
       <h1>Community</h1>
       <p>Discover work from the Skillshot community.</p>
